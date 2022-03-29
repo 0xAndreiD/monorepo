9998b01365944642ecc8b28a433aef66d32df141
MrCrocodileDev <luckycrocodile2261@gmail.com>
2022-03-28 22:55:29 -0700

    adding new login page

--- romeano/app/auth/components/LoginForm.tsx
+++ romeano/app/auth/components/LoginForm.tsx
@@ -3,15 +3,16 @@ import { LabeledTextField } from "app/core/components/LabeledTextField"
 import { Form, FORM_ERROR } from "app/core/components/Form"
 import login from "app/auth/mutations/login"
 import { Login } from "app/auth/validations"
+import Labeled from "app/core/components/generic/Labeled"
+import PoweredByRomeano from "app/core/assets/poweredRomeano"
 
 export const LoginForm = (props: { onSuccess?: () => void }) => {
   const router = useRouter()
   const [loginMutation] = useMutation(login)
 
   return (
-    <div>
-      <h1>Login</h1>
-
+    <div className="grid place-items-center mt-12">
+      <PoweredByRomeano alt="Romeano Logo" className="" />
       <Form
         submitText="Login"
         schema={Login}
@@ -31,18 +32,36 @@ export const LoginForm = (props: { onSuccess?: () => void }) => {
           }
         }}
       >
-        <LabeledTextField name="email" label="Email" placeholder="Email" />
-        <LabeledTextField name="password" label="Password" placeholder="Password" type="password" />
-        <div>
-          <Link href={Routes.ForgotPasswordPage()}>
-            <a>Forgot your password?</a>
+        <div className="gap-2 grid-rows-2">
+          <LabeledTextField name="email" label="Email" placeholder="Email" />
+          <LabeledTextField name="password" label="Password" placeholder="Password" type="password" />
+        </div>
+        <div className="grid-cols-2 gap-2 grid-rows-1 flex">
+          <Link href={Routes.SignupPage()}>
+            <button
+              type="submit"
+              className="inline-flex items-center px-12 py-2 border border-transparent text-sm font-medium rounded-md text-black bg-gray-200 hover:bg-gray-300 hover:text-white focus:outline focus:ring-2 focus:ring-offset-2 focus:ring-green-500"
+            >
+              Sign Up
+            </button>
           </Link>
+          <button
+            type="submit"
+            className="inline-flex items-center px-12 py-2 border border-transparent text-sm font-medium rounded-md text-white bg-black hover:bg-white hover:text-black hover:border-black focus:outline focus:ring-2 focus:ring-offset-2 focus:ring-green-500"
+          >
+            Sign In
+          </button>
         </div>
       </Form>
 
-      <div style={{ marginTop: "1rem" }}>
-        Or <Link href={Routes.SignupPage()}>Sign Up</Link>
-      </div>
+      <footer>
+        <div className="space-y-14"></div>
+        <div className="pt-60 align-bottom">
+          <Link href={Routes.ForgotPasswordPage()}>
+            <a>Forgot password</a>
+          </Link>
+        </div>
+      </footer>
     </div>
   )
 }
