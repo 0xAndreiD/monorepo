a07762869dbc19f78e121f162a53414183b17b21
MrCrocodileDev <luckycrocodile2261@gmail.com>
2022-07-12 18:13:57 -0700

    Fix lint

--- romeano/app/core/assets/CheckIcon.jsx
+++ romeano/app/core/assets/CheckIcon.jsx
@@ -1,6 +1,6 @@
 import React from "react"
 
-const CheckIcon = ({ className, size = 10, color = "green" }) => {
+const CheckIcon = ({ className, size = "12", color = "green" }) => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
