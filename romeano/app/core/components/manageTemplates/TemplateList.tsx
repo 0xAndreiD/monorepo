f5464a49b36cf90b178d655d552b533eac4ae3e4
MrCrocodileDev <luckycrocodile2261@gmail.com>
2022-07-14 22:53:13 -0700

    Fix TypeScript errors

--- romeano/app/core/components/manageTemplates/TemplateList.tsx
+++ romeano/app/core/components/manageTemplates/TemplateList.tsx
@@ -19,7 +19,7 @@ type Template = {
   proposalSubheading: string
   createdAt: any
   updatedAt: any
-  portalId: string
+  portalId: number
 }
 
 export function TemplateList(props: { data: Template[] }) {
@@ -71,7 +71,7 @@ export function TemplateList(props: { data: Template[] }) {
                       <td className="px-6 py-4 whitespace-nowrap">0</td>
                       <td className="px-6 py-4 items-center whitespace-nowrap">
                         <div className="flex justify-center gap-3">
-                          <BlitzLink href={Routes.EditCustomerPortal({ portalId: template.portalId })}>
+                          <BlitzLink href={Routes.EditCustomerPortal({ portalId: encodeHashId(template.portalId) })}>
                             <button
                               type="button"
                               className="items-center px-3 py-2 border border-gray-300 text-sm 
