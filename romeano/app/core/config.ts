e774879cbeeada0c152481e17caee1f366e6bc1d
MrCrocodileDev <luckycrocodile2261@gmail.com>
2022-07-20 15:04:50 -0700

    Fix URLs for stage

--- romeano/app/core/config.ts
+++ romeano/app/core/config.ts
@@ -1,8 +1,11 @@
 export const PUBLIC_PORT = 3000
 
 //todo - enable HTTPS and move the endpoint back to HTTPS
-export const BACKEND_ENDPOINT =
-  process.env.NODE_ENV === "production" ? "http://dev.romeano.com" : `http://localhost:${PUBLIC_PORT}`
+export const BACKEND_ENDPOINT = () => {
+  if (process.env.NODE_ENV === "production") return "http://app.romeano.com"
+  else if (process.env.NODE_ENV === "stage") return "http://stage.romeano.com:3000"
+  else return `http://localhost:${PUBLIC_PORT}`
+}
 export const PUBLIC_ROOT_URL =
   process.env.NODE_ENV === "development" ? "https://6866ccb5.ngrok.io" : "https://romeano.com"
 export const INTERNAL_UPLOAD_FS_PATH = "public/uploads"
