f06d6cbb3423040fa896fb5efb0d749a3bc6e16f
MrCrocodileDev <luckycrocodile2261@gmail.com>
2022-07-26 17:47:57 -0700

    Carousel fixes

--- romeano/app/core/components/generic/StakeholderApprovalCircles.tsx
+++ romeano/app/core/components/generic/StakeholderApprovalCircles.tsx
@@ -6,7 +6,10 @@ import { Stakeholder } from "../customerPortals/ProposalCard"
 import Carousel from "react-multi-carousel"
 import "react-multi-carousel/lib/styles.css"
 
-export function StakeholderApprovalCircles(props: { data: Array<Stakeholder & { eventCount?: number }> }) {
+export function StakeholderApprovalCircles(props: {
+  data: Array<Stakeholder & { eventCount?: number }>
+  carousel?: boolean
+}) {
   //used to set if the arrows are visible or not
   const [isShown, setIsShown] = useState(false)
 
@@ -25,7 +28,38 @@ export function StakeholderApprovalCircles(props: { data: Array<Stakeholder & { 
     },
   }
 
-  return (
+  const renderItems = (data) => {
+    return data.map((stakeholder, idx) => {
+      const initials = getInitialsOfName(stakeholder.firstName, stakeholder.lastName)
+      const colour = getColourFromString(initials)
+
+      return (
+        <div key={idx} className="grid grid-cols-1 grid-rows-1 items-top display-flex">
+          <div
+            className={`relative w-10 h-10 flex px-2 items-center justify-center
+                              ${colour} rounded-full`}
+          >
+            <span className="text-white static">{initials}</span>
+
+            {stakeholder.hasStakeholderApproved === true ? (
+              <div className="absolute top-7 left-7 h-4 w-4 rounded-full border-2 bg-green-500">
+                <CheckIcon className="text-white" />
+              </div>
+            ) : stakeholder.hasStakeholderApproved === false ? (
+              <div className="absolute top-7 left-7 h-4 w-4 rounded-full border-2 bg-red-500">
+                <XIcon className="text-white" />
+              </div>
+            ) : (
+              <div className="absolute top-7 left-7 h-4 w-4 rounded-full border-2 bg-gray-300" />
+            )}
+          </div>
+          {stakeholder.eventCount && <span className="text-sm">{stakeholder.eventCount}</span>}
+        </div>
+      )
+    })
+  }
+
+  return props.carousel === true ? (
     <div
       style={{ width: "100%", paddingRight: "50px" }}
       onMouseEnter={() => setIsShown(true)}
@@ -41,35 +75,10 @@ export function StakeholderApprovalCircles(props: { data: Array<Stakeholder & { 
         infinite={false}
         arrows={isShown}
       >
-        {props.data.map((stakeholder, idx) => {
-          const initials = getInitialsOfName(stakeholder.firstName, stakeholder.lastName)
-          const colour = getColourFromString(initials)
-
-          return (
-            <div key={idx} className="grid grid-cols-1 grid-rows-1 items-top display-flex">
-              <div
-                className={`relative w-10 h-10 flex px-2 items-center justify-center
-                                  ${colour} rounded-full`}
-              >
-                <span className="text-white static">{initials}</span>
-
-                {stakeholder.hasStakeholderApproved === true ? (
-                  <div className="absolute top-7 left-7 h-4 w-4 rounded-full border-2 bg-green-500">
-                    <CheckIcon className="text-white" />
-                  </div>
-                ) : stakeholder.hasStakeholderApproved === false ? (
-                  <div className="absolute top-7 left-7 h-4 w-4 rounded-full border-2 bg-red-500">
-                    <XIcon className="text-white" />
-                  </div>
-                ) : (
-                  <div className="absolute top-7 left-7 h-4 w-4 rounded-full border-2 bg-gray-300" />
-                )}
-              </div>
-              {stakeholder.eventCount && <span className="text-sm">{stakeholder.eventCount}</span>}
-            </div>
-          )
-        })}
+        {renderItems(props.data)}
       </Carousel>
     </div>
+  ) : (
+    <>{renderItems(props.data)}</>
   )
 }
