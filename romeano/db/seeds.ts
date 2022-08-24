06c0a5b667f3ed858d85054ac935fe43f4e4e301
MrCrocodileDev <luckycrocodile2261@gmail.com>
2022-08-24 13:39:31 -0700

    Typescript

--- romeano/db/seeds.ts
+++ romeano/db/seeds.ts
@@ -279,39 +279,39 @@ const seedCustomerPortal = async () => {
   const documentsRaw = [
     {
       portal: { connect: { id: portal.id } },
-      vendor: { connect: { id: portal.vendorId } },
+      vendor: { connect: { id: portal.vendorId! } },
       link: {
         create: {
           body: "Security Questionnaire",
           href: "security-questionnaire.txt",
           type: LinkType.Document,
-          vendor: { connect: { id: aeUser.vendorId } },
+          vendor: { connect: { id: aeUser.vendorId! } },
           creator: { connect: { id: aeUser.id } },
         },
       },
     },
     {
       portal: { connect: { id: portal.id } },
-      vendor: { connect: { id: portal.vendorId } },
+      vendor: { connect: { id: portal.vendorId! } },
       link: {
         create: {
           body: "Vendor Setup",
           href: "vendor-setup.txt",
           type: LinkType.Document,
-          vendor: { connect: { id: aeUser.vendorId } },
+          vendor: { connect: { id: aeUser.vendorId! } },
           creator: { connect: { id: aeUser.id } },
         },
       },
     },
     {
       portal: { connect: { id: portal.id } },
-      vendor: { connect: { id: portal.vendorId } },
+      vendor: { connect: { id: portal.vendorId! } },
       link: {
         create: {
           body: "W-9 Form",
           href: "w9.txt",
           type: LinkType.Document,
-          vendor: { connect: { id: aeUser.vendorId } },
+          vendor: { connect: { id: aeUser.vendorId! } },
           creator: { connect: { id: stakeholders[0].id } },
         },
       },
@@ -330,7 +330,7 @@ const seedCustomerPortal = async () => {
           body: "Proposal Doc",
           href: "proposal.txt",
           type: LinkType.Document,
-          vendor: { connect: { id: aeUser.vendorId } },
+          vendor: { connect: { id: aeUser.vendorId! } },
           creator: { connect: { id: aeUser.id } },
         },
       },
@@ -406,7 +406,7 @@ const seedCustomerPortal = async () => {
               body: linkElem.body,
               href: linkElem.href,
               type: LinkType.WebLink,
-              vendor: { connect: { id: aeUser.vendorId } },
+              vendor: { connect: { id: aeUser.vendorId! } },
               creator: { connect: { id: aeUser.id } },
             },
           })
@@ -507,7 +507,7 @@ async function seedPortalDetails() {
       stakeholder: {
         create: {
           jobTitle: "Director",
-          vendor: { connect: { id: portal.vendorId } },
+          vendor: { connect: { id: portal.vendorId! } },
         },
       },
       userPortals: {
@@ -523,13 +523,13 @@ async function seedPortalDetails() {
   await db.portalDocument.create({
     data: {
       portal: { connect: { id: portal.id } },
-      vendor: { connect: { id: portal.vendorId } },
+      vendor: { connect: { id: portal.vendorId! } },
       link: {
         create: {
           body: "portal2doc",
           href: "portal2doc.txt",
           type: LinkType.Document,
-          vendor: { connect: { id: aeUser.vendorId } },
+          vendor: { connect: { id: aeUser.vendorId! } },
           creator: { connect: { id: aeUser.id } },
         },
       },
@@ -544,7 +544,7 @@ async function seedPortalDetails() {
           body: "Proposal Doc",
           href: "proposal2.txt",
           type: LinkType.Document,
-          vendor: { connect: { id: aeUser.vendorId } },
+          vendor: { connect: { id: aeUser.vendorId! } },
           creator: { connect: { id: aeUser.id } },
         },
       },
@@ -683,7 +683,7 @@ async function seedMira() {
           body: "Proposal",
           href: "https://docs.google.com/presentation/d/1Kwh6nVp00qBtFVegGe-6iWCB4kstwJi4NjUbm5F4CI4/edit#slide=id.gd4d09b3e56_0_233",
           type: LinkType.WebLink,
-          vendor: { connect: { id: aeUser.vendorId } },
+          vendor: { connect: { id: aeUser.vendorId! } },
           creator: { connect: { id: aeUser.id } },
         },
       },
@@ -700,12 +700,12 @@ async function seedMira() {
           body: "Mira Product Video",
           href: "https://vimeo.com/364410995",
           type: LinkType.WebLink,
-          vendor: { connect: { id: aeUser.vendorId } },
+          vendor: { connect: { id: aeUser.vendorId! } },
           creator: { connect: { id: aeUser.id } },
         },
       },
       portal: { connect: { id: portal.id } },
-      vendor: { connect: { id: portal.vendorId } },
+      vendor: { connect: { id: portal.vendorId! } },
     },
     {
       heading: "Live AR Headset Demo",
@@ -715,24 +715,24 @@ async function seedMira() {
           body: "Join Zoom 📞",
           href: "https://www.google.com/webhp?client=firefox-b-d",
           type: LinkType.WebLink,
-          vendor: { connect: { id: aeUser.vendorId } },
+          vendor: { connect: { id: aeUser.vendorId! } },
           creator: { connect: { id: aeUser.id } },
         },
       },
       portal: { connect: { id: portal.id } },
-      vendor: { connect: { id: portal.vendorId } },
+      vendor: { connect: { id: portal.vendorId! } },
     },
     {
       heading: "Use-Case & Value Stream Workshop",
       tasks: ["Define primary use-case where Mira will be used", "Build interaction web", "Value map/ROI calculations"],
       portal: { connect: { id: portal.id } },
-      vendor: { connect: { id: portal.vendorId } },
+      vendor: { connect: { id: portal.vendorId! } },
     },
     {
       heading: "Deployment Configuration",
       tasks: ["Quantify deployment size & scope"],
       portal: { connect: { id: portal.id } },
-      vendor: { connect: { id: portal.vendorId } },
+      vendor: { connect: { id: portal.vendorId! } },
     },
   ]
 
@@ -803,7 +803,7 @@ async function seedMira() {
               body: linkElem.body,
               href: linkElem.href,
               type: LinkType.WebLink,
-              vendor: { connect: { id: aeUser.vendorId } },
+              vendor: { connect: { id: aeUser.vendorId! } },
               creator: { connect: { id: aeUser.id } },
             },
           })
