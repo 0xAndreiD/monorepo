87245eb3a2a4357ffaa872b9e38a4b8e27c502a1
MrCrocodileDev <luckycrocodile2261@gmail.com>
2022-12-12 09:50:45 -0800

    Anonymous layout

--- romeano/app/core/layouts/Layout.tsx
+++ romeano/app/core/layouts/Layout.tsx
@@ -59,4 +59,18 @@ const Layout = ({ title, children }: LayoutProps) => {
   )
 }
 
+export const AnonymousLayout = ({ title, children }: LayoutProps) => {
+  return (
+    <>
+      <Head>
+        <title>{title || "romeano"}</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div className="mt-16">{children}</div>
+      </div>
+    </>
+  )
+}
+
 export default Layout
